bc64ef6d888734ec6f39f082bc37a48c9999ac8b
Zach Robles <zrobles1188@gmail.com>
2023-08-05 16:42:49 -0400

    min max numbers

--- src/minimumAndMaximum.js
+++ src/minimumAndMaximum.js
@@ -1,11 +1,33 @@
 /**
  * Implement an algorithm that sorts the array then returns the minimum and maximum
  */
-function minimumAndMaximumSort(numbers) {}
+function minimumAndMaximumSort(numbers) {
+  numbers.sort((a, b) => a - b);
+  const first = numbers[0];
+  const last = numbers[numbers.length - 1];
+  return [first, last].filter((e) => e !== undefined);
+}
 
 /**
  * Implement an algorithm that uses iteration to find the minimum and maximum
  */
-function minimumAndMaximumIterate(numbers) {}
+function minimumAndMaximumIterate(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  }
+
+  let smallest = numbers[0];
+  let largest = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < smallest) {
+      smallest = numbers[i];
+    }
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return [smallest, largest];
+}
 
 module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
--- src/missingNumber.js
+++ src/missingNumber.js
@@ -1,11 +1,27 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
-function missingNumberBruteForce(numbers) {}
+function missingNumberBruteForce(numbers) {
+  let missingNumber = 0;
+  for (let i = 1; i <= numbers.length + 1; i++) {
+    if (numbers.indexOf(i) === -1) {
+      missingNumber = i;
+    }
+  }
+  return missingNumber;
+}
 
 /**
  * Use an iterative  strategy for finding the missing number in an array
  */
-function missingNumberSum(numbers) {}
+function missingNumberSum(numbers) {
+  const n = numbers.length + 1;
+  const expected = (n * (n + 1)) / 2;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return expected - sum;
+}
 
 module.exports = { missingNumberBruteForce, missingNumberSum };
